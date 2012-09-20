81ec60a4f865566ca8955d25066a4722b4134990
Sonicrida <Sonicrida@live.com>
2012-09-20 11:38:56 -0400

    Added comment timestamps
    Shows the time distance between the comment creation date and the
    article creation date.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'a46c2dfb12d806ef57328d6892669e802db91a4d4b6270ca31853408b1c710266d8b6ca29e91d6fcff99d79bddf9f5185115ba84bf5a7d4d5d598a9a2a9e960a'
